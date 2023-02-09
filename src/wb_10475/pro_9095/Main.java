8573c0baa13571ffa8466de1da51565d01503e80
HanHyunSoo <gustn4563@gmail.com>
2023-02-09 15:54:12 +0900

    solve: 9095

--- src/wb_10475/pro_9095/Main.java
+++ src/wb_10475/pro_9095/Main.java
@@ -1,7 +1,27 @@
 package pro_9095;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i ++) {
+            sb.append(dp[Integer.parseInt(br.readLine())]).append('\n');
+        }
+
+        System.out.println(sb);
     }
 }
