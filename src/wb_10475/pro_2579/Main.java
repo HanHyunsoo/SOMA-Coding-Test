8bebf28f794ac7abe6b2de183b472cc7830d2a72
HanHyunSoo <gustn4563@gmail.com>
2023-02-09 15:23:24 +0900

    solve: 2579

--- src/wb_10475/pro_2579/Main.java
+++ src/wb_10475/pro_2579/Main.java
@@ -1,7 +1,28 @@
 package pro_2579;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] stairs = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = stairs[1];
+        if (N >= 2) {
+            dp[2] = stairs[2] + stairs[1];
+        }
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(stairs[i - 1] + dp[i - 3], dp[i - 2]) + stairs[i];
+        }
+
+        System.out.println(dp[N]);
     }
 }
