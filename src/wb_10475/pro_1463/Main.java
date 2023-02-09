e93ecf518bcfc9fa6e4eb132cea2ad48afca0dc6
HanHyunSoo <gustn4563@gmail.com>
2023-02-09 14:53:37 +0900

    solve: 1463

--- src/wb_10475/pro_1463/Main.java
+++ src/wb_10475/pro_1463/Main.java
@@ -1,7 +1,31 @@
 package pro_1463;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] dp = new int[1000001];
+
+        dp[2] = 1;
+        dp[3] = 1;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
     }
 }
