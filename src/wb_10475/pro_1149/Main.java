19351820cadf02dc638ae2de1b81a0f0c6606928
HanHyunSoo <gustn4563@gmail.com>
2023-02-21 02:29:29 +0900

    solve: 1149

--- src/wb_10475/pro_1149/Main.java
+++ src/wb_10475/pro_1149/Main.java
@@ -1,7 +1,31 @@
 package pro_1149;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] rgb = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
+            rgb[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        int[][] dp = new int[N][3];
+        dp[0][0] = rgb[0][0];
+        dp[0][1] = rgb[0][1];
+        dp[0][2] = rgb[0][2];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
+        }
+
+        System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
     }
 }
