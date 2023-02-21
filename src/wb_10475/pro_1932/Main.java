199896c2504b5653ef4ebdda23c54c3e35808050
HanHyunSoo <gustn4563@gmail.com>
2023-02-21 09:26:06 +0900

    solve: 1932

--- src/wb_10475/pro_1932/Main.java
+++ src/wb_10475/pro_1932/Main.java
@@ -1,7 +1,33 @@
 package pro_1932;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < input.length; j++) {
+                dp[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] += dp[i - 1][j];
+                } else if (j == i) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] += Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp[n - 1]).max().getAsInt());
     }
 }
